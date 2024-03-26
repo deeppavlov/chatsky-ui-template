2183ead8f3d801124fe6d057536d7fbd6c2dea8d
Ramimashkouk <ramima1996@gmail.com>
2024-03-26 12:21:55 +0300

    feat: Add builds and runs

--- {{ cookiecutter.project_slug }}/app.py
+++ {{ cookiecutter.project_slug }}/app.py
@@ -1,8 +1,19 @@
 import logging
+from pathlib import Path
+import click
 
-from bot.pipeline import pipeline
+from dff import Pipeline
 
 
 logging.basicConfig(level=logging.INFO)
 
-pipeline.run()
+
+@click.command()
+@click.option('--script-path', required=True, help='Path to the script file')
+def main(script_path: str):
+    script_file = Path(__file__).parent / script_path
+    pipeline = Pipeline.from_file(script_file)
+    pipeline.run()
+
+if __name__ == "__main__":
+    main()
