2a3446624193bb1fdd900aa856f982d3df2cccfd
Ramimashkouk <ramima1996@gmail.com>
2024-03-27 14:46:44 +0300

    allow run_scenario to take build_id as argument

--- {{ cookiecutter.project_slug }}/runs/success_run.sh
+++ {{ cookiecutter.project_slug }}/runs/success_run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-dflowd run_scenario --project-dir .
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 BUILD_ID"
+    exit 1
+fi
+
+# Assign the first argument to the variable
+build_id=$1
+
+dflowd run_scenario $build_id --project-dir .
 # poetry run df_d run
