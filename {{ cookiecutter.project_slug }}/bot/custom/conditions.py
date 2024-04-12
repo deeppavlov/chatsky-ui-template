d2a8d74da39bf8a678c958fd84d3f0760b0952d5
Ramimashkouk <ramima1996@gmail.com>
2024-04-12 11:38:27 +0300

    chore: update default condition

--- {{ cookiecutter.project_slug }}/bot/custom/conditions.py
+++ {{ cookiecutter.project_slug }}/bot/custom/conditions.py
@@ -1,5 +1,5 @@
 from dff.script import Context
+from dff import Pipeline
 
-
-def is_upper_case(ctx: Context, pipeline):
+def is_upper_case(ctx: Context, pipeline: Pipeline):
     return ctx.last_request.text.isupper()
