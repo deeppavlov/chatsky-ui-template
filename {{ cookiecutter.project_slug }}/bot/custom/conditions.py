8c28d3e5478ad5493b2c1f05ddd6c1b01d73cd19
Ramimashkouk <ramima1996@gmail.com>
2024-06-11 11:12:45 +0300

    chore: Import cnd and Message in conditions.py

--- {{ cookiecutter.project_slug }}/bot/custom/conditions.py
+++ {{ cookiecutter.project_slug }}/bot/custom/conditions.py
@@ -1,5 +1,7 @@
 from dff.script import Context
 from dff import Pipeline
+from dff.script import Context, TRANSITIONS, RESPONSE, Message
+import dff.script.conditions as cnd
 
 def is_upper_case(ctx: Context, pipeline: Pipeline):
     return ctx.last_request.text.isupper()
