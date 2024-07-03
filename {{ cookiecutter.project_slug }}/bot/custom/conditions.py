76e34debbaa12aa794fa514138c2a730170ee60f
Ramimashkouk <ramima1996@gmail.com>
2024-07-04 00:23:18 +0300

    add responses.py file

--- {{ cookiecutter.project_slug }}/bot/custom/conditions.py
+++ {{ cookiecutter.project_slug }}/bot/custom/conditions.py
@@ -2,5 +2,8 @@ from dff import Pipeline
 from dff.script import Context, TRANSITIONS, RESPONSE, Message
 import dff.script.conditions as cnd
 
+
 def is_upper_case(ctx: Context, pipeline: Pipeline):
     return ctx.last_request.text.isupper()
+
+
