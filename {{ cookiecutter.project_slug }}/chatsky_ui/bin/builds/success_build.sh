39bbafa81554c5e26590cb068b0c5f58ac34fac3
Ramimashkouk <ramima1996@gmail.com>
2024-08-17 03:03:29 +0800

    fix bash files

--- {{ cookiecutter.project_slug }}/chatsky_ui/bin/builds/success_build.sh
+++ {{ cookiecutter.project_slug }}/chatsky_ui/bin/builds/success_build.sh
@@ -6,6 +6,6 @@ fi
 
 # Assign the first argument to the variable
 build_id=$1
-project_dir=$(dirname $(dirname $(realpath $0)))
+project_dir=$(dirname $(dirname $(dirname $(dirname $(realpath $0)))))
 
 chatsky.ui build_scenario $build_id --project-dir $project_dir
