7c8ec19357c0fc4c3afa684ed706bcbde27ba96b
Ramimashkouk <ramima1996@gmail.com>
2024-11-08 14:41:14 +0800

    chore: Add build conditions

--- {{ cookiecutter.project_slug }}/bot/custom/conditions.py
+++ {{ cookiecutter.project_slug }}/bot/custom/conditions.py
@@ -11,3 +11,18 @@ from chatsky import (
     conditions as cnd,
 )
 
+
+class hello_cnd(BaseCondition):
+
+    async def call(self, ctx: Context) -> bool:
+        return await cnd.ExactMatch('hello')(ctx)
+
+class yes_cnd(BaseCondition):
+
+    async def call(self, ctx: Context) -> bool:
+        return await cnd.ExactMatch('yes')(ctx)
+
+class no_cnd(BaseCondition):
+
+    async def call(self, ctx: Context) -> bool:
+        return await cnd.ExactMatch('no')(ctx)
