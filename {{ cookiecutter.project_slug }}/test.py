a63e10091f10d29d9fb0256e3880efc075ae7951
Ramimashkouk <ramima1996@gmail.com>
2024-11-12 15:48:44 +0800

    test: Get test.py to work

--- {{ cookiecutter.project_slug }}/test.py
+++ {{ cookiecutter.project_slug }}/test.py
@@ -1,25 +1,20 @@
 from pathlib import Path
 
 from chatsky import Pipeline
-from chatsky.utils.testing import (
-    check_happy_path,
-    is_interactive_mode,
-)
+from chatsky.utils.testing import check_happy_path
 
-script_path = Path(__file__).parent / "bot/scripts/build_0.yaml"
+script_path = Path(__file__).parent / "bot/scripts/build.yaml"
 custom_dir = Path(script_path).parent.parent / "custom"
 pipeline = Pipeline.from_file(file = script_path, custom_dir = custom_dir)
 
 HAPPY_PATH = [
-    ("Hi", "Do you want pizza?"),
-    ("Yes", "Great! Would you like cheese on it?")
-    ("Yes", "Order placed"),
+    ("Hi", "Hello!"),
+    ("hello", "Do you want a pizza?"),
+    ("yes", "Some cheese in pizza?"),
+    ("yes", "Cool! Your order is coming!"),
 ]
 
 
-assert check_happy_path(
+check_happy_path(
         pipeline, HAPPY_PATH, printout=True
     )
-
-# if is_interactive_mode():
-#     pipeline.run()
