717de2137808296620f8ebeacba3612321af5337
Ramimashkouk <ramima1996@gmail.com>
2024-11-28 21:25:35 +0800

    fix: Delete build_id from builds

--- {{ cookiecutter.project_slug }}/test.py
+++ {{ cookiecutter.project_slug }}/test.py
@@ -8,7 +8,7 @@ custom_dir = Path(script_path).parent.parent / "custom"
 pipeline = Pipeline.from_file(file = script_path, custom_dir = custom_dir)
 
 HAPPY_PATH = [
-    ("Hi", "Hello!"),
+    ("/start", "Hello!"),
     ("hello", "Do you want a pizza?"),
     ("yes", "Some cheese in pizza?"),
     ("yes", "Cool! Your order is coming!"),
