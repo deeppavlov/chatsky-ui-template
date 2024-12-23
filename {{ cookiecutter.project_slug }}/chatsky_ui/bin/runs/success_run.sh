2cef450279f80eda02b42d674d1c2266ee712d9c
Ramimashkouk <ramima1996@gmail.com>
2024-12-23 14:06:41 +0800

    remove build id from cli cmd

--- {{ cookiecutter.project_slug }}/chatsky_ui/bin/runs/success_run.sh
+++ {{ cookiecutter.project_slug }}/chatsky_ui/bin/runs/success_run.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 BUILD_ID"
-    exit 1
-fi
-
-# Assign the first argument to the variable
-build_id=$1
 project_dir=$(dirname $(dirname $(dirname $(dirname $(realpath $0)))))
 
-chatsky.ui run_scenario $build_id --project-dir $project_dir
+chatsky.ui run_scenario --project-dir $project_dir
