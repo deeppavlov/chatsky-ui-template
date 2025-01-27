7eb76d10e54c3773d529cdea7cf1befe0a953cb4
Ramimashkouk <ramima1996@gmail.com>
2025-01-27 17:47:45 +0800

    fix: Pass chatsky-port only if it has a value

--- {{ cookiecutter.project_slug }}/chatsky_ui/bin/builds/success_build.sh
+++ {{ cookiecutter.project_slug }}/chatsky_ui/bin/builds/success_build.sh
@@ -1,3 +1,7 @@
 project_dir=$(dirname $(dirname $(dirname $(dirname $(realpath $0)))))
 
-chatsky.ui build_scenario --project-dir $project_dir
+if [ -z "$chatsky_port" ]; then
+    chatsky.ui build_scenario --project-dir $project_dir
+else
+    chatsky.ui build_scenario --project-dir $project_dir --chatsky-port $chatsky_port
+fi
