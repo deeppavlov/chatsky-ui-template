a0b3e6337cb29318655c5ef32dd8164ddfd43436
Ramimashkouk <ramima1996@gmail.com>
2025-02-04 23:38:26 +0800

    replace interface with messenger & add it to sh

--- {{ cookiecutter.project_slug }}/chatsky_ui/bin/builds/success_build.sh
+++ {{ cookiecutter.project_slug }}/chatsky_ui/bin/builds/success_build.sh
@@ -1,7 +1,7 @@
 project_dir=$(dirname $(dirname $(dirname $(dirname $(realpath $0)))))
 
 if [ -z "$chatsky_port" ]; then
-    chatsky.ui build_scenario --project-dir $project_dir
+    chatsky.ui build_scenario --project-dir $project_dir --messenger $messenger
 else
-    chatsky.ui build_scenario --project-dir $project_dir --chatsky-port $chatsky_port
+    chatsky.ui build_scenario --project-dir $project_dir --messenger $messenger --chatsky-port $chatsky_port
 fi
