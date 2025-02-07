0d975d3c71d3d02734353a0ade97b68e5c3027f1
Ramimashkouk <ramima1996@gmail.com>
2025-02-07 19:16:27 +0800

    return build_id to build_scenario

--- {{ cookiecutter.project_slug }}/chatsky_ui/bin/builds/success_build.sh
+++ {{ cookiecutter.project_slug }}/chatsky_ui/bin/builds/success_build.sh
@@ -1,7 +1,7 @@
 project_dir=$(dirname $(dirname $(dirname $(dirname $(realpath $0)))))
 
 if [ -z "$chatsky_port" ]; then
-    chatsky.ui build_scenario --project-dir $project_dir --messenger $messenger
+    chatsky.ui build_scenario $build_id --project-dir $project_dir --messenger $messenger
 else
-    chatsky.ui build_scenario --project-dir $project_dir --messenger $messenger --chatsky-port $chatsky_port
+    chatsky.ui build_scenario $build_id --project-dir $project_dir --messenger $messenger --chatsky-port $chatsky_port
 fi
