47eb5d821b3654796055b54ec5caac387dcb183f
ZergLev <vasilev.sv@phystech.edu>
2025-02-18 15:48:28 +0300

    added run_id to main() arguments for saving context_storages separately

--- {{ cookiecutter.project_slug }}/app.py
+++ {{ cookiecutter.project_slug }}/app.py
@@ -5,6 +5,7 @@ import click
 
 from chatsky import Pipeline
 from chatsky.context_storages import context_storage_factory
+from chatsky_ui.core.config import settings
 
 
 logging.basicConfig(level=logging.INFO)
@@ -12,9 +13,10 @@ logging.basicConfig(level=logging.INFO)
 
 @click.command()
 @click.option("--script-path", required=True, help="Path to the script file")
-def main(script_path: Path):
+def main(script_path: Path, run_id: int = 0):
     separator = "///" if system() == "Windows" else "////"
-    db_uri = f"sqlite+aiosqlite:{separator}{Path('dbs/sqlite.db').absolute()}"
+    db_path = Path(f"{settings.context_storage_dir}/run_{run_id}.db")
+    db_uri = f"sqlite+aiosqlite:{separator}{db_path.absolute()}"
     db = context_storage_factory(db_uri)
 
     pipeline = Pipeline.from_file(
