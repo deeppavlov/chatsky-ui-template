0060fa226eb8e1129991b7a144ef94fb5333b89c
ZergLev <64711614+ZergLev@users.noreply.github.com>
2025-03-17 12:06:33 +0300

    refactor: removed comment, decided in a call to keep dialogue-db-path parameter

--- {{ cookiecutter.project_slug }}/app.py
+++ {{ cookiecutter.project_slug }}/app.py
@@ -18,11 +18,7 @@ logging.basicConfig(level=logging.INFO)
 def main(script_path: Path, dialogue_db_path: Path, run_id: int):
     separator = "///" if system() == "Windows" else "////"
 
-    # TODO: (decide before merge) 'dialogue_db_path.absolute' could just be settings.database_path,
-    # which removes an unnecessary parameter, making the code easier to read.
-    # But I'm not sure if that's correct, so I placed this comment here.
     db_uri = f"sqlite+aiosqlite:{separator}{dialogue_db_path.absolute()}"
-    # db = context_storage_factory(db_uri)
     db = ChatskyUIContextStorage(db_uri, run_id)
 
     pipeline = Pipeline.from_file(
