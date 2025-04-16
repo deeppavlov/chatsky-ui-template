254c1fdaabf02a59bd6b57ce5cb4b6ced6f91ded
ZergLev <64711614+ZergLev@users.noreply.github.com>
2025-04-16 09:50:53 +0300

    feat: Add SQLite Context Storage (#3)
    * SQLite context_storage added to Pipeline
    
    * added run_id to main() arguments for saving context_storages separately
    
    * ensured db_file exists, removed redundant file
    
    * fix: passing run_id to run_scenario
    
    * added a test.db for testing SQLExtractor in the backend
    
    * fix: configured app.py click options for passing run_id
    
    * refactor: renamed test DB for tests to work
    
    * fix: Get db path as a parameter
    
    ** Like this, the path will be relative to the working-dir instead of repo root
    
    * feat: added the ChatskyUIContextStorage class and it's description, added it's use in app.py
    
    * refactor: removed redundant comment
    
    * refactor: renamed the test database, fix: forgot to add import
    
    * fix: wrong imports, should be good now
    
    * fix: imported SQLContextStorage from the wrong Chatsky directory
    
    * fix: forgot to import Chatsky Context type
    
    * fix: forgot to add cmd parameter 'run_id' to app.py
    
    * refactor: changed run_id type back to int
    
    * fix: forgot to actually pass the run_id
    
    * fix: new context storage class function parameter was wrong
    
    * fix: added 'awaits' for async functions in the new class
    
    * test (remove before merge)
    
    * test (remove before merge)
    
    * test
    
    * test
    
    * test
    
    * test
    
    * fix: forgot to return the results from relevant functions in the new class
    
    * refactor: removed comment, decided in a call to keep dialogue-db-path parameter
    
    * style: removed redundant import
    
    * refactor: removed ChatskyUIContextStorage from the template, moving it to the chatsky-ui repo
    
    * feat: emptied db for the update to Chatsky v0.10.0, need to recreate tests though
    
    * feat: test base updated to Chatsky v0.10.0
    
    * refactor: Delete redundunt click parameters
    
    ---------
    
    Co-authored-by: Ramimashkouk <ramima1996@gmail.com>
    Co-authored-by: Rami Mashkouk <rami.n.mashkouk@gmail.com>

--- {{ cookiecutter.project_slug }}/app.py
+++ {{ cookiecutter.project_slug }}/app.py
@@ -1,17 +1,30 @@
 import logging
 from pathlib import Path
+from platform import system
 import click
 
 from chatsky import Pipeline
+from chatsky_ui.clients.context_storage import ChatskyUIContextStorage
 
 
 logging.basicConfig(level=logging.INFO)
 
 
 @click.command()
-@click.option("--script-path", required=True, help="Path to the script file")
-def main(script_path: Path):
-    pipeline = Pipeline.from_file(file = script_path, custom_dir = Path(script_path).parent.parent / "custom")
+@click.option("--working-dir", required=True, type=Path, help="Path to the script file")
+@click.option("--run-id", required=True, type=int, help="ID of this `Run` process")
+def main(working_dir: Path, run_id: int):
+    working_dir = Path(working_dir)
+    separator = "///" if system() == "Windows" else "////"
+
+    db_uri = f"sqlite+aiosqlite:{separator}{working_dir.absolute() / 'bot' / 'databases' / 'context_storage.db'}"
+    db = ChatskyUIContextStorage(db_uri, run_id)
+
+    pipeline = Pipeline.from_file(
+        file=working_dir / "bot" / "scripts" / "build.yaml",
+        custom_dir=working_dir / "bot" / "custom",
+        context_storage=db,
+    )
     pipeline.run()
 
 
